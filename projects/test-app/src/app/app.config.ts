3fe76a170bef6ff0d68e3a081b988a4436c0e768
antistereov <andre.antimonov@stereov.io>
2025-04-02 12:01:51 +0200

    fix(demo-app): use demo routes in demo project

--- projects/test-app/src/app/app.config.ts
+++ projects/test-app/src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import {provideBaseline} from '@baseline/core/config/base.config';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-      provideBaseline()
+      provideBaseline(),
+      provideRouter(routes)
   ]
 };
