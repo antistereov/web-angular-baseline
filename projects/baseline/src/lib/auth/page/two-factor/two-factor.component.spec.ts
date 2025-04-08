a45e54642b9b678d399077e644883f592f8adb7e
antistereov <andre.antimonov@stereov.io>
2025-04-09 01:38:35 +0200

    test: fix test setup for two-factor

--- projects/baseline/src/lib/auth/page/two-factor/two-factor.component.spec.ts
+++ projects/baseline/src/lib/auth/page/two-factor/two-factor.component.spec.ts
@@ -1,23 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TwoFactorComponent } from './two-factor.component';
+import {ActivatedRoute, convertToParamMap} from "@angular/router";
+import {provideHttpClient} from "@angular/common/http";
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from "@ngx-translate/core";
 
 describe('TwoFactorComponent', () => {
-  let component: TwoFactorComponent;
-  let fixture: ComponentFixture<TwoFactorComponent>;
+    let component: TwoFactorComponent;
+    let fixture: ComponentFixture<TwoFactorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TwoFactorComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        const mockActivatedRoute = {
+            snapshot: {
+                queryParamMap: convertToParamMap({ redirect: '/' }),
+                paramMap: convertToParamMap({ context: 'login' })
+            }
+        };
 
-    fixture = TestBed.createComponent(TwoFactorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            imports: [
+                TwoFactorComponent,
+                TranslateModule.forRoot({
+                    loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
+                })
+            ],
+            providers: [
+                { provide: ActivatedRoute, useValue: mockActivatedRoute },
+                provideHttpClient()
+            ]
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(TwoFactorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
