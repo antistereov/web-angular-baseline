c4285e59777681527559ce7562c820dec8e33b97
antistereov <admin@stereov.io>
2025-04-10 00:20:24 +0200

    chore(two-factor-setup): add QR code with TOPT URL

--- projects/baseline/src/lib/auth/page/two-factor-setup/two-factor-setup.component.ts
+++ projects/baseline/src/lib/auth/page/two-factor-setup/two-factor-setup.component.ts
@@ -8,13 +8,23 @@ import {catchError, Observable, tap, throwError} from "rxjs";
 import {AuthCardComponent} from "@baseline/auth/ui/auth-card/auth-card.component";
 import {TranslatePipe} from "@ngx-translate/core";
 import {NgIf} from "@angular/common";
+import {QrCodeComponent} from "ng-qrcode";
+import {InputOtpComponent} from "@baseline/shared/ui/component/input-otp/input-otp.component";
+import {FormsModule} from "@angular/forms";
+import {DividerComponent} from "@baseline/shared/ui/component/divider/divider.component";
+import {Checkbox} from "primeng/checkbox";
 
 @Component({
   selector: 'base-two-factor-setup',
     imports: [
         AuthCardComponent,
         TranslatePipe,
-        NgIf
+        NgIf,
+        QrCodeComponent,
+        InputOtpComponent,
+        FormsModule,
+        DividerComponent,
+        Checkbox
     ],
   templateUrl: './two-factor-setup.component.html',
 })
@@ -26,6 +36,7 @@ export class TwoFactorSetupComponent {
 
     private redirectTo: string;
 
+    code: string = '';
     setupLoaded: boolean = false;
     userLoaded = this.userService.userLoaded;
     twoFactorInfo = toSignal(this.setup());
